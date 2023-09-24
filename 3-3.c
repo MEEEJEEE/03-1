e22e24f6d859e2f5a450ac494d5815b19ebccaba
MIJI LEE <144990941+MEEEJEEE@users.noreply.github.com>
2023-09-24 22:21:51 +0900

    Update 3-3.c

--- 3-3.c
+++ 3-3.c
@@ -4,13 +4,13 @@ int main() {
     char inputChar;
     
     // 사용자로부터 알파벳 문자를 입력 받음
-    printf("알파벳 문자를 입력하세요: ");
+    printf("enter a character : ");
     scanf(" %c", &inputChar); // 공백 문자를 사용하여 이전 입력 버퍼를 비웁니다.
 
     // 입력 받은 문자에 1을 더하여 다음 문자를 계산하고 출력
     char nextChar = inputChar + 1;
 
-    printf("다음 문자는: %c\n", nextChar);
+    printf("the next character is : %c\n", nextChar);
 
     return 0;
 }
